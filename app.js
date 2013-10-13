512cd04e5400bd2e589946919249bc928721a96a
Richard C Isaacson <richard@eucalyptus.com>
2013-10-12 23:19:41 -0500

    Change default port to 8080 and clean up commented value.

--- app.js
+++ app.js
@@ -12,12 +12,11 @@ var express = require('express')
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8080);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
-// app.use(express.bodyParser());
 app.use(express.bodyParser({ keepExtensions: true, uploadDir: "uploads" }));
 app.use(express.methodOverride());
 app.use(app.router);
